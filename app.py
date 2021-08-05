bd13e938d9a139f66863b7faf7971d45ea53c1cf
Firaze <fvattiato9@gmail.com>
2021-08-05 19:51:44 +0200

    First

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+from functions import *
+import PIL
+#Network(notebook=True)
+
+# makes Network show itself with repr_html
+pathways_name=pd.read_csv("data/pathways.tsv", sep='\t')["pathway_name"]
+
+#excange of pathway to make easier the findings of the (interesting) pathway on the 25th position 
+tmp=pathways_name.iloc[0]
+pathways_name.iloc[0]=pathways_name.iloc[25]
+pathways_name.iloc[25]=tmp
+
+st.set_page_config(layout="wide")
+
+#populating sidebar
+st.sidebar.title('Choose a pathway')
+option=st.sidebar.selectbox('',pathways_name)
+normal_edges=st.sidebar.checkbox('Show expression/suppression edges')
+removed_edges=st.sidebar.checkbox('Show removed edges',value=True)
+st.sidebar.text("Edge legend:")
+w=25
+h=5
+red = np.zeros((h, w, 3), dtype=np.uint8)
+red[0:]=[255, 0, 0]
+green = np.zeros((h, w, 3), dtype=np.uint8)
+green[0:]=[0, 128, 0]
+blue = np.zeros((h, w, 3), dtype=np.uint8)
+blue[0:]=[30,144,255]
+yellow = np.zeros((h, w, 3), dtype=np.uint8)
+yellow[0:]=[255, 255,0]
+st.sidebar.image(blue, caption='Expression edges')
+st.sidebar.image(yellow, caption='Suppression edges')
+st.sidebar.image(green, caption='Part of triad edges')
+st.sidebar.image(red, caption='Removed edges')
+
+
+#skip_calcs if an invalid pathways was selected
+skip_calcs=False
+#selects pathway
+pathway_edges=read_pathway(option)
+#checks if the pathway is invalid
+if (len(pathway_edges)==0):
+    #option=last_selection
+    skip_calcs=True
+    st.error("Edges not found, try another pathway!")
+else:
+    skip_calcs=False
+    #last_selection=option
+    pathway_edges=read_pathway(option)
+    #calculates the adj_matrix and the dictionary to find the nodes, with new names, in the matrix
+    adj_matrix,nodes_renamed,inv_nodes_renamed=build_adj(pathway_edges)
+    G = nx.from_numpy_matrix(adj_matrix)
+    #gets the triads
+    triad_cliques=get_triad(G)
+    #does the SEM analysis, weight every edge and remove any invalid triad in the list of cliques
+    weighted_edges,triad_cliques=calculate_weighted_edges(triad_cliques, adj_matrix,inv_nodes_renamed)
+    #calculate which edges must be removed, the edges that must be kept and all their significative values 
+    to_remove, signify_values, essential_edges=evaluate_edges(weighted_edges)
+    relabel={}
+    #relabel the nodes of the graph to obtain the real names of the genes
+    for e,node in enumerate( G.nodes()):
+        relabel[e]=str(inv_nodes_renamed[node])
+    net=Network(height="825px",notebook=True,directed=True,width="1800px", bgcolor='#222222', font_color='white')
+    
+    #filters the nodes if the user wants to show only triad results
+    triad_nodes=set()
+    _=[triad_nodes.add(str(inv_nodes_renamed[y])) for x in triad_cliques for y in x]
+    triad_nodes=list(triad_nodes)
+    
+    #adds nodes in the grapg
+    for i,node in relabel.items():
+        if normal_edges:
+            net.add_node(str(node))
+        #add only triad nodes if the user wants triad related results
+        elif node in triad_nodes:
+            net.add_node(str(node))
+    #same for the edges
+    if (normal_edges):
+        for edge in pathway_edges.values:
+                if(edge[2]==-1):
+                    net.add_edge(str(edge[0]), str(edge[1]), color="yellow")
+                else:
+                    net.add_edge(str(edge[0]), str(edge[1]))
+    #colors the edge of green if it is not removed, red otherwise
+    #also assigns the values, calculated some lines above, to each edge
+    for triad in triad_cliques:
+        for i,x in enumerate(triad):
+            for j,y in enumerate(triad):
+                value=""
+                isessential=""
+                tmp=pathway_edges[(pathway_edges[0]==inv_nodes_renamed[triad[i]]) & (pathway_edges[1]==inv_nodes_renamed[triad[j]])].values
+                if (len(tmp)>0):
+                    start_node,to_node,weight=tmp[0]
+                else:
+                    continue
+                if ((str(start_node)+","+str(to_node)) in to_remove): 
+                    if (removed_edges==False):
+                        continue
+                    color="red"
+                    size=3
+                    value+=", significativity:  "+str(signify_values[str(start_node)+","+str(to_node)])
+                else:
+                    color="green"
+                    size=3
+                    value+=", significativity:  "+str(signify_values[str(start_node)+","+str(to_node)])
+                if ((str(start_node)+","+str(to_node)) in essential_edges):   
+                    isessential="Essential "
+                if (weight==1):
+                    net.add_edge(str(start_node), str(to_node), color=color, width=size,title=isessential+"Expression edge"+value)
+                else:
+                    net.add_edge(str(start_node), str(to_node), color=color, width=size,title=isessential+"Suppression edge"+value)
+    #sets the garph
+    net.hrepulsion(node_distance=120, central_gravity=0.0, spring_length=100, spring_strength=0, damping=0.09)
+    net.show("data/graph.html")
+    #reads and shows the graph
+    HtmlFile = open("data/graph.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 850,width=1850)
+
---
+++ functions.py
@@ -0,0 +1,177 @@
+import semopy
+from semopy import Model
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import networkx as nx
+from matplotlib import pyplot as plt
+#read and process file
+pathways=pd.read_csv("data/pathways.tsv", sep='\t')
+esets=pd.read_csv("data/controls_counts_norm.csv")
+esets.index=esets["Unnamed: 0"].values
+esets=esets.drop(columns="Unnamed: 0")
+gene_edges=pd.read_csv("data/gene_edges.tsv", sep='\t')
+
+
+
+#given a selected pathway, return the processed edges of the pathway
+def read_pathway(pathway_name):
+    pathway_id=np.where(pathways["pathway_name"]==pathway_name)[0]
+    apathway=pathways["nodes"].loc[int(pathway_id)]
+    apathway=apathway.split(";")
+    apathway=[int(x) for x in apathway]
+    pathway_edges=pd.DataFrame([list(x) for x in gene_edges.values if x[0] in apathway])
+    pathway_edges=pd.DataFrame([x for x in pathway_edges.values if str(x[0]) in esets.index])
+    pathway_edges=pd.DataFrame([x for x in pathway_edges.values if str(x[1]) in esets.index])
+    return pathway_edges
+
+#calculates the adj_matrix and the dictionary to find the nodes, with new names, in the matrix
+def build_adj(pathway_edges):
+    pathway_edges_0=pathway_edges[0].unique()
+    pathway_edges_1=pathway_edges[1].unique()
+    nodes=list(np.hstack((pathway_edges_0,pathway_edges_1)))
+    nodes_renamed={}
+    inv_nodes_renamed={}
+    for e,x in enumerate(nodes):
+        nodes_renamed[x]=e
+        inv_nodes_renamed[e]=x
+    nodes=len(nodes)
+    adj_matrix=np.zeros((nodes,nodes))
+    for x in pathway_edges.values:
+        adj_matrix[nodes_renamed[x[0]]][nodes_renamed[x[1]]]=x[2]
+    return (adj_matrix,nodes_renamed,inv_nodes_renamed)
+
+#gets the triads
+def get_triad(G):
+    all_cliques= nx.enumerate_all_cliques(G)
+    triad_cliques=[x for x in all_cliques if len(x)==3 ]
+    return triad_cliques
+
+
+#does the SEM analysis, weight every edge and remove any invalid triad in the list of cliques
+def calculate_weighted_edges(triad_cliques, adj_matrix,inv_nodes_renamed):
+    weighted_edges={}
+    new_triad_cliques=[]
+    first_label=""
+    second_label=""
+    third_label=""
+    #SEM model
+    mod = """ 
+              y ~ x1 + x2
+              """
+    #for each triad
+    for triad in range(len(triad_cliques)):
+        triad_matrix=np.zeros((3,3))
+        for i,x in enumerate(triad_cliques[triad]):
+            for j,y in enumerate(triad_cliques[triad]):
+                triad_matrix[i][j]=adj_matrix[x][y]
+        zeros_count=np.array([len(np.where(x==0)[0]) for i,x in enumerate(triad_matrix) ])
+        #filtering any invalid clique
+        if (sum(zeros_count)==6):
+            new_triad_cliques.append(triad_cliques[triad])
+        else:
+            continue
+        #the first is always the rows of the matrix with two edges (one 0 in the matrix) to the other genes
+        #the same goes for the other two genes (one edge for the second gene and zero for the third)
+        first_index=int(np.where(zeros_count==1)[0])
+        second_index=int(np.where(zeros_count==2)[0])
+        third_index=int(np.where(zeros_count==3)[0])
+        #gets the gene expression of each gene
+        first_label=str(inv_nodes_renamed[triad_cliques[triad][first_index]])
+        second_label=str(inv_nodes_renamed[triad_cliques[triad][second_index]])
+        third_label=str(inv_nodes_renamed[triad_cliques[triad][third_index]])
+        first_gene=(list(esets.loc[first_label,:].values),0)
+        second_gene=(list(esets.loc[second_label,:].values),1)
+        third_gene=(list(esets.loc[third_label,:].values),2)
+
+        y=third_gene
+        x1=first_gene
+        x2=second_gene
+        to_df={"y":y[0],"x1":x1[0],"x2":x2[0]}
+        data=pd.DataFrame(to_df).replace(np.inf, np.nan).replace(-np.inf, np.nan).dropna()
+        m = Model(mod)
+        #SEM analysis
+        r = m.fit(data)
+        #sum of latent variables found by SEM
+        fac_sum=np.abs(r.x[0]+r.x[1])
+        #check if the edge between the first and the third node is less significant than the others two, this edge will be weighted as 1,
+        #the other two as 0
+        if (np.abs(r.x[0])<fac_sum*0.1):
+            if (first_label+","+third_label in weighted_edges):
+                weighted_edges[first_label+","+third_label].append((r.x[0],0))
+
+            else:
+                weighted_edges[first_label+","+third_label]=[(r.x[0],0)]
+            if (second_label+","+third_label in weighted_edges) :
+                weighted_edges[second_label+","+third_label].append((r.x[1],1))
+            else:
+                weighted_edges[second_label+","+third_label]=[(r.x[1],1)]
+
+            if(first_label+","+second_label in weighted_edges) :
+                weighted_edges[first_label+","+second_label].append((r.x[1],1))
+            else:
+                weighted_edges[first_label+","+second_label]=[(r.x[1],1)]
+        #the other two are less significant
+        elif(np.abs(r.x[1])<fac_sum*0.1):
+            if (first_label+","+third_label in weighted_edges):
+                weighted_edges[first_label+","+third_label].append((r.x[0],1))
+
+            else:
+                weighted_edges[first_label+","+third_label]=[(r.x[0],1)]
+            if (second_label+","+third_label in weighted_edges):  
+                weighted_edges[second_label+","+third_label].append((r.x[1],0))
+            else:
+                weighted_edges[second_label+","+third_label]=[(r.x[1],0)]
+            if(first_label+","+second_label in weighted_edges) :
+                weighted_edges[first_label+","+second_label].append((r.x[1],0))
+            else:
+                weighted_edges[first_label+","+second_label]=[(r.x[1],0)]
+        #there are no most significant edge in the clique, all edges will be weighted as -1
+        else:
+            if (first_label+","+third_label in weighted_edges):
+                weighted_edges[first_label+","+third_label].append((r.x[0],-1))
+
+            else:
+                weighted_edges[first_label+","+third_label]=[(r.x[0],-1)]
+            if (second_label+","+third_label in weighted_edges):  
+                weighted_edges[second_label+","+third_label].append((r.x[1],-1))
+            else:
+                weighted_edges[second_label+","+third_label]=[(r.x[1],-1)]
+            if(first_label+","+second_label in weighted_edges) :
+                weighted_edges[first_label+","+second_label].append((r.x[1],-1))
+            else:
+                weighted_edges[first_label+","+second_label]=[(r.x[1],-1)]
+    return weighted_edges, new_triad_cliques
+
+def evaluate_edges(weighted_edges):
+    to_remove=[]
+    signify_values={}
+    essential_edges=[]
+    #for each weighted edge
+    for x in weighted_edges.items():
+        zeros=0
+        ones=0
+        minus=0
+        for z in x[1]:
+            if (z[1]==0):
+                zeros+=1
+            elif (z[1]==1):
+                ones+=1
+            else:
+                minus+=1
+        if (ones==0):
+            if (minus==0):
+                to_remove.append(x[0])
+            else:
+                m=(zeros+minus)/2
+                #remove crition
+                if ((minus+zeros)/(zeros*minus+1)*zeros/(minus+1)>((minus+zeros)/(m*m+1))*zeros/(minus+1)):
+                    to_remove.append(x[0])
+        else:
+            essential_edges.append(x[0])
+        if (ones==0):
+            signify_values[x[0]]=round((minus+zeros)/(zeros*minus+1)*(zeros)/(minus+1),3)
+        else:
+            signify_values[x[0]]=0
+    return to_remove, signify_values, essential_edges
+
